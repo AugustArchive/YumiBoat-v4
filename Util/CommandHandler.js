d361ee02fea6f3916f8275d9288b604ae288c13d
August (Chris) <27751995+ohlookitsAugust@users.noreply.github.com>
2017-10-09 16:58:28 -0700

    [Production | CommandFilePath] :)

---
+++ Util/CommandHandler.js
@@ -0,0 +1,36 @@
+const File = require('file');
+const fs = require('fs');
+
+class CommandFilePath {
+  constructor() {
+    super();
+    this.log = require('./Logger');
+    this.client = client;
+  }
+  
+  File.walk(this.path, (err, dirP, dir) => {
+     if (err) {
+       this.log.error(`[Command Path] Error while processing with this:\n\nError Message: ${err.message}\nError Stack: ${err.stack}`);
+     } else {
+        dir.forEach(dirPath => {
+          fs.readdir(dirPath, (error, files) => {
+            if (error) {
+              throw new Error(error);
+            } else {
+              files.forEach(filez => {
+                let Command = require(`../Commands/${folder}/$[filez}`);
+                let Module = new Command(this.client, folder);
+                this.client.commands.set(Module.name, Module);
+                this.log.notify(`[Command Path] Loading ${Module.name} from ${folder}!`);
+                Module.aliases.forEach(alias => {
+                  this.client.aliases.set(alias, Module.name);
+                });
+              });
+            }
+          });
+        });
+     }
+  });
+}
+
+module.exports = CommandFilePath;
